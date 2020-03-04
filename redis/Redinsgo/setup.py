d3412bb11f0d264cef8b7e93a7b816f5ffe6247e
João Luiz Fernandes <joao.fernandes@ellevo.com>
2020-03-04 20:02:32 -0300

    start game

--- redis/Redinsgo/setup.py
+++ redis/Redinsgo/setup.py
@@ -8,6 +8,7 @@ redis_con = redis.Redis(host='localhost', port=6380, db=0, password="Jasinski")
 redis_con.flushall()
 redis_con.sadd("stones", *range(1, 100))
 
+#setup
 pipe = redis_con.pipeline()
 for i in range(50):
     useri = user+str(i)
@@ -22,6 +23,28 @@ pipe.execute()
 
 
 
-#client.sadd(user.cartelaKey, user.cartela);
-#client.set(user.scoreKey, 0);
+#game start
+turn = 0
+while stone := redis_con.spop("stones", 1):
+    turn = turn + 1
+    bingo = False;
+    intStone = int(stone[0])
+    print("turn: " + str(turn) + ", stone: " + str(intStone))
 
+    for i in range(50):
+        if redis_con.sismember(cartela + str(i), intStone):
+            pipe.incr(score + str(i))
+    pipe.execute()
+
+    for i in range(50):
+        if int(redis_con.get(score + str(i))) == 15:
+            print("Bingo: " + user + str(i) + "!")
+            bingo = True
+
+    if bingo:
+        break;
+
+if not bingo:
+    "Acabaram as pedras!"
+
+pipe.close()
