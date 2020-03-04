d215d4b1eb0dd0c809e1e005b65f9810c1026e5f
João Luiz Fernandes <joao.fernandes@ellevo.com>
2020-03-04 17:36:20 -0300

    gitignore

--- redis/Redinsgo/setup.py
+++ redis/Redinsgo/setup.py
@@ -1,6 +1,6 @@
 import redis
 
-r = redis.Redis(host='localhost', port=6379, db=0, password="DCU];h}X.CT6wL]M")
+r = redis.Redis(host='localhost', port=6379, db=0, password="Jasinski")
 r.flushall()
 
 #r.set('foo', 'bar')
