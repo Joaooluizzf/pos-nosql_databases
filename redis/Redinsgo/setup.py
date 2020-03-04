1d8db9e06bea5947853ce093cd09620df294c207
João Luiz Fernandes <joao.fernandes@ellevo.com>
2020-03-04 18:52:51 -0300

    setup do bingo

--- redis/Redinsgo/setup.py
+++ redis/Redinsgo/setup.py
@@ -1,8 +1,27 @@
 import redis
 
-r = redis.Redis(host='localhost', port=6379, db=0, password="Jasinski")
-r.flushall()
+user = "user:"
+score = "score:"
+cartela = "cartela:"
 
-#r.set('foo', 'bar')
-#r.get('foo')
-#r.incr('incr')
+redis_con = redis.Redis(host='localhost', port=6380, db=0, password="Jasinski")
+redis_con.flushall()
+redis_con.sadd("stones", *range(1, 100))
+
+pipe = redis_con.pipeline()
+for i in range(50):
+    useri = user+str(i)
+    cartelai = cartela + str(i)
+    scorei = score + str(i)
+    pipe.hset(useri, "name", useri)
+    pipe.hset(useri, "bcartela", cartelai)
+    pipe.hset(useri, "bscore", scorei)
+    pipe.sadd(cartelai, *redis_con.srandmember("stones", 15))
+    pipe.set(scorei, 0)
+pipe.execute()
+
+
+
+#client.sadd(user.cartelaKey, user.cartela);
+#client.set(user.scoreKey, 0);
+
